7e4b5fd1355c29b4941b488ad6352dd27a5e09b3
OGD97 <omardaya1997@hotmail.com>
2023-11-16 19:59:28 +0300

    Just a dice game

---
+++ dice.js
@@ -0,0 +1,87 @@
+
+var ran1 = Math.floor((Math.random() * 6)+1);
+var ran2 = Math.floor((Math.random() * 6)+1);
+
+
+var dice1 = "images/dice1.png";
+var dice2 = "images/dice2.png";
+var dice3 = "images/dice3.png";
+var dice4 = "images/dice4.png";
+var dice5 = "images/dice5.png";
+var dice6 = "images/dice6.png";
+
+
+var img1 = document.getElementsByClassName("img1")[0];
+var img2 = document.getElementsByClassName("img2")[0];
+
+if(ran1 === 1)
+{
+  img1.src = dice1;
+}
+else if(ran1 === 2)
+{
+  img1.src = dice2;
+}
+else if(ran1 === 3)
+{
+  img1.src = dice3;
+}
+else if(ran1 === 4)
+{
+  img1.src = dice4;
+}
+else if(ran1 === 5)
+{
+  img1.src = dice5;
+}
+else if(ran1 === 6)
+{
+  img1.src = dice6;
+}
+
+
+
+
+if(ran2 === 1)
+{
+  img2.src = dice1;
+
+}
+else if(ran2 === 2)
+{
+  img2.src = dice2;
+}
+else if(ran2 === 3)
+{
+  img2.src = dice3;
+}
+else if(ran2 === 4)
+{
+  img2.src = dice4;
+}
+else if(ran2 === 5)
+{
+  img2.src = dice5;
+}
+else if(ran2 === 6)
+{
+  img2.src = dice6;
+}
+
+
+if(ran1 > ran2)
+{
+  document.querySelector("h1").innerHTML = "Player 1 WINS !";
+}
+else if(ran1 < ran2)
+{
+  document.querySelector("h1").innerHTML = "Player 2 WINS !";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw !";
+}
+//dice1 till dice6
+//randomize the dice
+//use if statements 
+//dice1, assign
